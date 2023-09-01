bcb391fc2a649107c6ad8234f2c4da232d333930
numoonchld <17598505+numoonchld@users.noreply.github.com>
2023-09-01 07:50:10 +0000

    stacks and queues

--- udemy_javascript_ds&algo/data-structures/linked-lists/00-linked-lists.md
+++ udemy_javascript_ds&algo/data-structures/linked-lists/00-linked-lists.md
@@ -1,8 +1,23 @@
 # linked lists
 
+## singly linked lists
+
 10 --> 5 --> 16
 
 - append
 - prepend
 - insert
 - remove
+- reverse
+
+## doubly linked lists
+
+
+# pros and cons
+
+- fast insertion and fast deletion
+- ordered
+- flexible size
+
+- slow lookup
+- more memory
--- udemy_javascript_ds&algo/data-structures/linked-lists/02-doubly-linked-lists.js
+++ udemy_javascript_ds&algo/data-structures/linked-lists/02-doubly-linked-lists.js
@@ -1,21 +1,9 @@
-// let myLinkedList = {
-//   head: {
-//     value: 10,
-//     next: {
-//       value: 5,
-//       next: {
-//         value: 16,
-//         next: null,
-//       }
-//     }
-//   }
-// }
-
-class LinkedList {
+class DoublyLinkedList {
   constructor(value) {
     this.head = {
       value: value,
-      next: null
+      next: null,
+      prev: null
     }
     this.tail = this.head;
     this.length = 1;
@@ -24,8 +12,10 @@ class LinkedList {
   append(value) {
     const newNode = {
       value: value,
-      next: null
+      next: null,
+      prev: null
     }
+    newNode.prev = this.tail;
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++
@@ -35,8 +25,11 @@ class LinkedList {
   prepend(value) {
     const newNode = {
       value: value,
-      next: this.head
+      next: null,
+      prev: null
     }
+    newNode.next = this.head;
+    this.head.prev = newNode
     this.head = newNode
     this.length++
     return this
@@ -62,15 +55,18 @@ class LinkedList {
     }
     const newNode = {
       value: value,
-      next: null
+      next: null,
+      prev: null
     }
 
     const leader = this.traverseToIndex(index - 1)
-    const holdingPointer = leader.next;
+    const follower = leader.next;
 
     leader.next = newNode
-    newNode.next = holdingPointer
-
+    newNode.prev = leader
+    newNode.next = follower
+    follower.prev = newNode
+    
     this.length++
     return this.printList()
   }
@@ -91,43 +87,21 @@ class LinkedList {
   remove(index) {
     const leader = this.traverseToIndex(index - 1)
     const unwantedNode = leader.next
-    leader.next = unwantedNode.next
-
+    unwantedNode.next.previous = leader;
+    leader.next = unwantedNode.next;
     this.length--
     
   }
-
-  reverse() {
-    if (!this.head.next) {
-      return this
-    }
-
-    let first = this.head
-    this.tail = this.tail
-    let second = first.next
-
-    while(second) {
-      const temp = second.next
-      second.next = first;
-      first = second 
-      second = temp
-    }
-
-    this.head.next = null
-    this.head = first
-  }
 }
 
-const myLinkedList = new LinkedList(10)
+const myLinkedList = new DoublyLinkedList(10)
 myLinkedList.append(5)
 myLinkedList.append(16)
 myLinkedList.prepend(1)
-// console.log(myLinkedList)
+console.log(myLinkedList)
 console.log(myLinkedList.printList())
 myLinkedList.insert(200,99)
 myLinkedList.insert(2,39)
 console.log(myLinkedList.printList())
-myLinkedList.remove(2)
-console.log(myLinkedList.printList())
-myLinkedList.reverse()
+myLinkedList.remove(3)
 console.log(myLinkedList.printList())
